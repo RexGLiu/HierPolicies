eee375409b6329fc7423482892c56ae3a9d44432
Rex Liu <49000792+RexGLiu@users.noreply.github.com>
2024-05-08 14:38:08 -0400

    Added further explanatory comments to ambig_env_run_par.py

--- Flat Task/ambig_env_run_par.py
+++ Flat Task/ambig_env_run_par.py
@@ -56,7 +56,7 @@ context_balance = [4] * len(context_goals)
 # context change after i repeats is f(i)
 hazard_rates = [0.5, 0.67, 0.67, 0.75, 1.0, 1.0]
 
-
+# collate task params into a dict
 task_kwargs = dict(context_balance=context_balance, 
                    context_goals=[goal_locations[g] for g in context_goals], 
                    context_maps=[mapping_definitions[m] for m in context_maps],
@@ -64,20 +64,21 @@ task_kwargs = dict(context_balance=context_balance,
                    grid_world_size=grid_world_size, list_goal_locations=goal_locations.values(),
                    )
 
-
 n_sims = 150 ## run 150 in the paper
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-n_procs = comm.Get_size()
-
+# collate agent params into a dict
 agent_kwargs = dict(discount_rate=0.75, inverse_temperature=5.0, alpha=1.0)
 meta_kwargs = dict(agent_kwargs)
 meta_kwargs['m_biases'] = [0.0, 0.0]  # log prior for Ind, Joint models, respectively
 hier_agent_kwargs = dict(agent_kwargs)
 del hier_agent_kwargs['alpha']
 
+# set-up parallel processing
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+n_procs = comm.Get_size()
 
+# randomly select random seeds for each processor in processor 0 and then scatter to all other processors
 if rank == 0:
     seed = 234234
     np.random.seed(seed)
@@ -88,15 +89,16 @@ else:
 seed = comm.scatter(rand_seeds, root=0)
 np.random.seed(seed)
 
+# compute number of trials to run on each processor
 q, r = divmod(n_sims, n_procs)
 if rank < r:
     n_local_sims = q + 1
-    sim_offset = n_local_sims*rank
+    sim_offset = n_local_sims*rank   # used for determining trial number
 else:
     n_local_sims = q
-    sim_offset = (q+1)*r + q*(rank-r)
+    sim_offset = (q+1)*r + q*(rank-r)   # used for determining trial number
 
-
+# lists for storing local sim results
 results_jc = [None] * n_local_sims
 results_ic = [None] * n_local_sims
 results_fl = [None] * n_local_sims
@@ -105,13 +107,14 @@ results_h = [None] * n_local_sims
 
 clusterings_h = [None] * n_local_sims
 
+# iterate through trials
 for kk in range(n_local_sims):
     sim_number = sim_offset+kk
     
-    task = make_task(**task_kwargs)
+    task = make_task(**task_kwargs)  # create task for current trial
     results_h[kk], clusterings_h[kk] = simulate_one(HierarchicalAgent, sim_number, task, agent_kwargs=hier_agent_kwargs)
 
-    task.reset()
+    task.reset()  # reset task and reuse for all other agents
     results_mx[kk], _ = simulate_one(MetaAgent, sim_number, task, agent_kwargs=meta_kwargs)
 
     task.reset()
@@ -123,7 +126,8 @@ for kk in range(n_local_sims):
     task.reset()
     results_fl[kk], _ = simulate_one(FlatControlAgent, sim_number, task, agent_kwargs=agent_kwargs)
 
-
+# gather all results to processor 0 for collation and saving
+# hierarchical agent results
 _results_h = comm.gather(results_h, root=0)
 if rank == 0:
     results_h = []
@@ -142,7 +146,7 @@ if rank == 0:
     pickle.dump( clusterings_h, open( "AmbigEnvClusterings_h_mixed.pkl", "wb" ) )
     del _clusterings_h, clusterings_h
 
-
+# mixed agent results
 _results_mx = comm.gather(results_mx, root=0)
 if rank == 0:
     results_mx = []
@@ -152,6 +156,7 @@ if rank == 0:
     results_mx['Model'] = ['Meta'] * len(results_mx)
     del _results_mx
 
+# joint agent results
 _results_jc = comm.gather(results_jc, root=0)
 if rank == 0:
     results_jc = []
@@ -161,6 +166,7 @@ if rank == 0:
     results_jc['Model'] = ['Joint'] * len(results_jc)
     del _results_jc
 
+# independent agent results
 _results_ic = comm.gather(results_ic, root=0)
 if rank == 0:
     results_ic = []
@@ -170,6 +176,7 @@ if rank == 0:
     results_ic['Model'] = ['Independent'] * len(results_ic)
     del _results_ic
 
+# flat agent results
 _results_fl = comm.gather(results_fl, root=0)
 if rank == 0:
     results_fl = []
